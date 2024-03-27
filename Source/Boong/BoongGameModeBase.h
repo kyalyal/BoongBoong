d446605451238213e95f625f7b92d22a8108026d
GONG-317-006\gong-317-005 <dbtmd1999@gmail.com>
2024-03-27 16:31:53 +0900

    초기 프로젝트

---
+++ Source/Boong/BoongGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BoongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOONG_API ABoongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
