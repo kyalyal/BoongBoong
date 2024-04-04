e417fc1aa4321588a286a2e4ffc7c762fe1d80fb
GONG-317-007\MJC <lgj415415@naver.com>
2024-04-04 17:16:59 +0900

    cpp 파일 작업시작

---
+++ Source/Boong/BoongGameInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "BoongGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOONG_API UBoongGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	// 보기만 하는거
+	// 캐릭터 받아올 변수
+	// 가비지..
+	
+};
