1cf9a0a635773bb86f07d85d475df2cbf6fb224a
GONG-317-007\MJC <lgj415415@naver.com>
2024-04-04 18:23:01 +0900

    BoongGameInstance에 변수 추가

--- Source/Boong/BoongGameInstance.h
+++ Source/Boong/BoongGameInstance.h
@@ -6,12 +6,31 @@
 #include "Engine/GameInstance.h"
 #include "BoongGameInstance.generated.h"
 
+
+//Player 자동차 식별 변수만들기
+//Player Level 
+//Player 생명
+//삼인칭 C++
+//플러그인으로 C++코딩이 되어있는 차의 블루프린트.
+
+
+// 상기widget 기어 속도
+
+
+//죽었을때
+
 /**
  * 
  */
 UCLASS()
 class BOONG_API UBoongGameInstance : public UGameInstance
 {
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString CarName;
+	int32 PlayerLevel;
+	int32 PlayerLife = 100;
+
 	GENERATED_BODY()
 
 	// 보기만 하는거
