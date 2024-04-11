8c67241356c6aac4c0db9d0e3048df2383587497
junseungwon <zkdlman1@gmail.com>
2024-04-11 22:43:29 +0900

    함수명 수정

--- Source/Boong/BoongGameModeBase.cpp
+++ Source/Boong/BoongGameModeBase.cpp
@@ -4,23 +4,23 @@
 #include "BoongGameModeBase.h"
 #include "Engine.h"
 #include "Kismet/GameplayStatics.h"
+#include "Blueprint/UserWidget.h"
 
+//메인레벨로 전환한다.
 void ABoongGameModeBase::ChangeMainLevel()
 {
     //상기 LSK_Test_Start_Level // 건주Main_Circit
     UGameplayStatics::OpenLevel(this, "Main_Circit");
 }
-
+//처음시작레벨로 전환한다.
 void ABoongGameModeBase::ChangeStartLevel()
 {
     UGameplayStatics::OpenLevel(this,"LSK_Test_Start_Level");
 }
-
+//플레이어 생성
 void ABoongGameModeBase::ReSpawn()
 {
     
-    
-     
 }
 //제한시간 타이머를 실행한다.
 void ABoongGameModeBase::StartTimer()
@@ -43,19 +43,73 @@ void ABoongGameModeBase::StartTimer()
      PlayTimeMin = PlayTime / 60;
      PlayTimeSec = PlayTime % 60;
      UE_LOG(LogTemp, Log, TEXT("Remaining Time: %02d:%02d"), PlayTimeMin, PlayTimeSec);
-     // 타이머 종료 시 TimeUp을 호출하고 타이머 핸들을 초기화한다.
-     if (PlayTime <= 0)
+     // 타이머 종료 시 실격처리하고 타이머 핸들을 초기화한다.
+     if (PlayTime <= 0) 
      {
-         TimeUp();
+         TestDisqualification();
          StopPlayerTimer();
      }
  }
+ //PlayTime을 10분으로 초기화한다.
+ void ABoongGameModeBase::ResetPlayTime()
+ {
+     PlayTime = 600;
+ }
 
- void ABoongGameModeBase::TimeUp()
+ //5점이 감점이 되고 실격점수 60점보다 낮아지면 실격처리가 된다.
+ void ABoongGameModeBase::MinusPlayerScore()
+ {
+     PlayerScore -= 5;
+     if (PlayerScore < 60) 
+     {
+         TestDisqualification();
+         StopPlayerTimer();
+     }
+
+ }
+
+ //플레이어의 점수가 100점으로 리셋된다.
+ void ABoongGameModeBase::ResetPlayerScore()
+ {
+     PlayerScore = 100;
+ }
+
+ //도착선에 도달시 플레이어가 합격인지 불합격인지 판단한다. 
+ void ABoongGameModeBase::JudgePass()
+ {
+     StopPlayerTimer();
+     if (PlayerScore >= 80) //점수 80점이 넘으면 통과
+     {
+         PlayerPass();
+     }
+     else //아닐경우 불합격처리
+     {
+         PlayerFail();
+     }
+ }
+ //#블루프린트로 해당 UI를 가져와서 설정해야함
+ //플레이어가 합격 했을 때
+ void ABoongGameModeBase::PlayerPass()
+ {
+    
+ }
+ //플레이어가 불합격 했을 때
+ void ABoongGameModeBase::PlayerFail()
  {
 
  }
 
+ //시험 실격처리가 되었을 때
+ void ABoongGameModeBase::TestDisqualification()
+ {
+
+ }
+ /*
+ void ABoongGameModeBase::TestTest()
+ {
+     CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), MainWidget);
+     CurrentWidget->AddToViewport();
+ }*/
  void ABoongGameModeBase::BeginPlay()
  {    
      Super::BeginPlay();
--- Source/Boong/BoongGameModeBase.h
+++ Source/Boong/BoongGameModeBase.h
@@ -14,7 +14,8 @@ class BOONG_API ABoongGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 public :
-	int32 Speed = 0;//카트 속력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimerValue")
+	int32 PlayerScore = 100; // 플레이어 점수
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimerValue")
 	int32 PlayTime = 600;
@@ -25,14 +26,18 @@ public :
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimerValue")
 	int32 PlayTimeSec = 0;
 
+	int32 Speed = 0;//카트 속력
 	FTimerHandle TimerHandle; //제한시간 타이머를 식별하기 위한 핸들
 public :
+
 	//메인레벨로 전환한다.
 	UFUNCTION(BlueprintCallable)
 	virtual void ChangeMainLevel();
+
 	//처음시작레벨로 전환한다.
 	UFUNCTION(BlueprintCallable)
 	virtual void ChangeStartLevel();
+
 	//플레이어 재생성
 	virtual void ReSpawn();
 
@@ -40,14 +45,49 @@ public :
 	UFUNCTION(BlueprintCallable)
 	virtual void StartTimer();
 	
+	UFUNCTION(BlueprintCallable)
 	//타이머 핸들을 멈추거나 초기화할 때 사용한다.
 	virtual void StopPlayerTimer();
 	
+	UFUNCTION(BlueprintCallable)
 	//타이머의 시간을 계산하여 분과 초의 변수에 값을 삽입한다.
 	virtual void CalculateTimer();
+
+	//PlayTime을 10분으로 초기화한다.
+	virtual void ResetPlayTime();
 	
-	//제한시간이 종료되었을 경우 발생
-	virtual void TimeUp();
+	UFUNCTION(BlueprintCallable)
+	//플레이어의 점수가 5점 감점이 된다.
+	virtual void MinusPlayerScore();
+
+	UFUNCTION(BlueprintCallable)
+	//플레이어의 점수가 100점으로 리셋된다.
+	virtual void ResetPlayerScore();
+
+	UFUNCTION(BlueprintCallable)
+	//도착선에 도달시 플레이어가 합격인지 불합격인지 판단한다. 
+	virtual void JudgePass();
+
+	UFUNCTION(BlueprintCallable)
+	//플레이어가 합격 했을 때
+	virtual void PlayerPass();
+
+	UFUNCTION(BlueprintCallable)
+	//플레이어가 불합격 했을 때
+	virtual void PlayerFail();
+	
+	UFUNCTION(BlueprintCallable)
+	//실격처리가 되었을 때
+	virtual void TestDisqualification();
+
+	/*
+	UFUNCTION(BlueprintCallable)
+	virtual void TestTest();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimerValue")
+	TSubclassOf<UUserWidget> MainWidget;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimerValue")
+	UUserWidget* CurrentWidget;
+	*/
 protected:
 	virtual void BeginPlay() override;
 
