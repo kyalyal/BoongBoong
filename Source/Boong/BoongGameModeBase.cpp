9bba892ce0f52346cd6496560a27258804528e88
junseungwon <zkdlman1@gmail.com>
2024-04-17 23:41:46 +0900

    코드 테스트

--- Source/Boong/BoongGameModeBase.cpp
+++ Source/Boong/BoongGameModeBase.cpp
@@ -103,7 +103,7 @@ void ABoongGameModeBase::StartTimer()
  void ABoongGameModeBase::TestCode()
  {
      ///Script/UMGEditor.WidgetBlueprint'/Game/SW/Blueprint/DeadUi.DeadUi'
-     FStringClassReference MyWidget = (TEXT("/ Script / UMGEditor.WidgetBlueprint'/Game/SW/Blueprint/DeadUi.DeadUi_C'"));
+     FStringClassReference MyWidget = (TEXT("'/Game/SW/Blueprint/DeadUi.DeadUi_C'"));
      
      UClass* SwWidget = MyWidget.TryLoadClass<UUserWidget>();
      
